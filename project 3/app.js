68239e13443bd7a43c522105e066e12a65f057d0
Ryan Dugan <70173704+RyanDugan@users.noreply.github.com>
2023-10-22 15:53:18 -0500

    added js

--- project 3/app.js
+++ project 3/app.js
@@ -1,1 +1,15 @@
 console.log("Hello World!");
+
+const circle = document.querySelector("#circle");
+
+circle.addEventListener("mouseenter", () => {
+  if (!circle.classList.contains("hover")) {
+    circle.classList.add("hover");
+  }
+});
+
+circle.addEventListener("mouseleave", () => {
+  if (circle.classList.contains("hover")) {
+    circle.classList.remove("hover");
+  }
+});
