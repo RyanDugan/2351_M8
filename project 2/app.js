3babbafa2e985f2562fc8ad071d4f1ac36566e5d
Ryan Dugan <70173704+RyanDugan@users.noreply.github.com>
2023-10-22 15:26:15 -0500

    added an array

--- project 2/app.js
+++ project 2/app.js
@@ -1,1 +1,28 @@
 console.log("Hello World!");
+
+let data = [
+  {
+    name: "Matero",
+    age: "30",
+  },
+  {
+    name: "Sarah",
+    age: "32",
+  },
+  {
+    name: "John",
+    age: "20",
+  },
+  {
+    name: "Tim",
+    age: "27",
+  },
+  {
+    name: "Sam",
+    age: "22",
+  },
+  {
+    name: "Joey",
+    age: "34",
+  },
+];
